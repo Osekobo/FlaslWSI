e5ccb383efedef4b1f670d047f60e922cc3f2f71
Osekobo <collinsboseko2005@gmail.com>
2025-09-22 18:11:52 +0300

    templateinheritance

--- main.py
+++ main.py
@@ -3,12 +3,8 @@ from flask import Flask, redirect, url_for, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def landing():
-  return "Hello! this is the landing page"
-
-@app.route("/home")
 def home():
-  return render_template("base.html", content=["tim", "joe", "bill"])
+  return render_template("index.html")
 
 if __name__ == "__main__":
-  app.run()
+  app.run(debug=True)
